ebf4bd507c54ad9c28a9d9a4b7dd9f27c8504cc2
Gary Shirk <garyshirk@yahoo.com>
2017-02-20 23:42:39 -0600

    test

--- ThriftStoreLocator/MainViewController.swift
+++ ThriftStoreLocator/MainViewController.swift
@@ -15,7 +15,7 @@ class MainViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Uncomment the following line to preserve selection between presentations
+        // Uncomment the following to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
 
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
