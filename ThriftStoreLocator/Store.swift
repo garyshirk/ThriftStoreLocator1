9e71c586da08d093f9d17aca5d6bb90e83313d8a
Gary Shirk <garyshirk@yahoo.com>
2017-02-28 19:07:02 -0600

    fixed compile issue - entity module name must be entered in xxdatamodel, and generated entity+CoreDataClass renamed to Entity

---
+++ ThriftStoreLocator/Store.swift
@@ -0,0 +1,16 @@
+//
+//  Store+CoreDataClass.swift
+//  ThriftStoreLocator
+//
+//  Created by Gary Shirk on 2/28/17.
+//  Copyright © 2017 Gary Shirk. All rights reserved.
+//  This file was automatically generated and should not be edited.
+//
+
+import Foundation
+import CoreData
+
+@objc(Store)
+public class Store: NSManagedObject {
+
+}
